9a8e635eec1c5a7793ad5ef63b72cdc7ec646209
Teetwothefirst <dexterdavid835@gmail.com>
2024-10-22 06:54:05 -0700

    small changes to the header

--- DreamDwellReality/src/App.jsx
+++ DreamDwellReality/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import Header from './component/header.jsx'
+import Header from './component/header'
+// import header from './component/header'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     // <>
@@ -33,7 +34,11 @@ function App() {
     <>
       <div>
         <Header></Header>
-        change
+        {/* <header></header> */}
+        {/* change */}
+      </div>
+      <div>
+        
       </div>
     </>
   )
--- DreamDwellReality/src/component/header.jsx
+++ DreamDwellReality/src/component/header.jsx
@@ -1,7 +1,21 @@
+import bgImg from '../assets/bg-img.png'
+
 function header(){
-    <>
-        <div>
-            <h1 className="diplomata-regular">DreamDwell <br />Realty</h1>
+   return <>
+        <div className="relative">
+            <img src={bgImg} alt="" width={`100%`}/>
+            <div className='w-full px-5 absolute top-10 text-white'>
+                <div className='flex justify-between'>
+                <h1 className="diplomata-regular">DreamDwell <br />Realty</h1>
+                <div className='flex gap-5'>
+                    <a href="">Home</a>
+                    <a href="">Properties</a>
+                    <a href="">Services</a>
+                    <a href="">About Us</a>
+                    <a href="">Contact Us</a>
+                </div>
+                </div>
+            </div>
         </div>
     </>
 }
